22aba5eb63f036235f51cf48efc3f79e80129b8b
Ilya Pribylsky <maestro.aquarius@gmail.com>
2016-07-28 17:34:49 +1200

    Some refactoring + shader output cleaning tool

---
+++ modules/graphics/src/graphics/font/io/FontIO.cpp
@@ -0,0 +1,37 @@
+//---------------------------------------------------------------------------
+
+#include <graphics/font/io/FontIO.h>
+
+#include <iostream>
+
+//---------------------------------------------------------------------------
+
+namespace Rapture
+{
+	using std::ifstream;
+	using std::ios_base;
+
+	void FontIO::load(Handle<Font> & output, const path & filepath)
+	{
+		using namespace boost::filesystem;
+
+		auto raw = handle<OwnedByteData>();
+		path pth = filepath;
+
+		if(!exists(pth))
+			pth = path("C:/Windows/Fonts") / filepath;
+
+		if(!exists(pth))
+			throw Exception("File ", filepath.string(), " doesn't exist!");
+
+		ifstream input(pth.string(), ios_base::binary);
+
+		raw->alloc(static_cast<size_t>(file_size(pth)));
+		input.read(reinterpret_cast<char *>(raw->ptr), raw->size);
+
+		read(output, get_extension(pth), raw);
+	}
+
+}
+
+//---------------------------------------------------------------------------
